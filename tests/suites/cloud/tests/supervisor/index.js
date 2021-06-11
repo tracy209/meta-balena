cb906da0fe82f163bc152dc5d5d7eb34abd07ecd
rcooke-warwick <ryan@balena.io>
2021-06-11 13:26:16 +0000

    speed up tests
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- tests/suites/cloud/tests/supervisor/index.js
+++ tests/suites/cloud/tests/supervisor/index.js
@@ -21,27 +21,6 @@ module.exports = {
     {
       title: "Provisioning without deltas",
       run: async function (test) {
-        // push multicontainer app release to new app
-        test.comment(`Cloning repo...`);
-        await exec(
-          `git clone https://github.com/balena-io-examples/balena-python-hello-world.git ${__dirname}/app`
-        );
-
-        test.comment(`Pushing release...`);
-
-        let firstCommit = await this.context.get().cloud.pushReleaseToApp(
-          this.context.get().balena.application, 
-          `${__dirname}/app`
-        );
-
-        await waitUntilServicesRunning(
-          this,
-          this.context.get().balena.uuid, 
-          [`main`], 
-          firstCommit,
-          test
-        )
-
         test.comment(`Disabling deltas`);
         await this.context
           .get()
@@ -50,14 +29,14 @@ module.exports = {
             "BALENA_SUPERVISOR_DELTA",
             0
           );
-
-        // add a comment to the end of the main.py file, to trigger a delta
-        await exec(`echo "#comment" >> ${__dirname}/app/src/main.py`);
+        
+        // add a comment to the end of the server.js file, to trigger a delta when pushing
+        await exec(`echo "//comment" >> ${this.context.get().appPath}/server.js`);
         test.comment(`Pushing release...`);
 
         let secondCommit = await this.context.get().cloud.pushReleaseToApp(
           this.context.get().balena.application, 
-          `${__dirname}/app`
+          `${this.context.get().appPath}`
         );
 
         await waitUntilServicesRunning(
@@ -80,6 +59,15 @@ module.exports = {
           true,
           `Device shouldn't use deltas to download new release`
         );
+        
+        // re-enable deltas to save time later
+        await this.context
+        .get()
+        .cloud.balena.models.device.configVar.set(
+          this.context.get().balena.uuid,
+          "BALENA_SUPERVISOR_DELTA",
+          1
+        );
       },
     },
     {
@@ -96,14 +84,6 @@ module.exports = {
             `systemctl stop balena-supervisor && balena rm balena_supervisor && balena rmi -f $(balena images | grep supervisor | awk '{print $3}')`,
             this.context.get().balena.uuid
           );
-
-        // push an update to the application
-        test.comment(`Pushing release...`);
-        
-        let firstCommit = await this.context.get().cloud.pushReleaseToApp(
-          this.context.get().balena.application, 
-          `${__dirname}/../../app` // push original release to application (node hello world)
-        );
 
         // run supervisor update script
         test.comment(`running update supervisor script...`);
@@ -145,49 +125,27 @@ module.exports = {
           true,
           `Supervisor should now be running`
         );
-
-        await waitUntilServicesRunning(
-          this,
-          this.context.get().balena.uuid, 
-          [`main`], 
-          firstCommit,
-          test
-        )
-
-        test.ok(
-          true,
-          `Device should have downloaded services from original app`
-        );
       },
     },
     {
       title: "Override lock test",
       run: async function (test) {
-        test.comment(`Cloning repo...`);
-        await exec(
-          `git clone https://github.com/balena-io-examples/balena-updates-lock.git ${__dirname}/lock`
-        );
+        let firstCommit = await this.context.get().cloud.balena.models.application.get(
+          this.context.get().balena.application
+        ).get("commit");
 
-        test.comment(`Pushing release...`);
-        let firstCommit = await this.context.get().cloud.pushReleaseToApp(
-          this.context.get().balena.application, 
-          `${__dirname}/lock`
-        );
+        // create a lockfile
+        let createLockfile = await this.context.get().cloud.executeCommandInContainer(
+          `bash -c '(flock -x -n 200)200>/tmp/balena/updates.lock'`, 
+          `main`,
+          this.context.get().balena.uuid)
 
-        await this.context.get().utils.waitUntil(async () => {
-          test.comment("Checking if lockfile has been created...");
-          let lockfile = await this.context.get().cloud.executeCommandInContainer(
-            `ls /tmp/balena`, 
-            `main`,
-            this.context.get().balena.uuid)
-          return lockfile === `updates.lock`;
-        }, false, 50);
-
-        // push original application
+        // push release to application
+        await exec(`echo "//comment" >> ${this.context.get().appPath}/server.js`);
         test.comment(`Pushing release...`);
         let secondCommit = await this.context.get().cloud.pushReleaseToApp(
           this.context.get().balena.application, 
-          `${__dirname}/../../app` // push original release to application (node hello world)
+          `${this.context.get().appPath}` // push original release to application (node hello world)
         );
 
         // check original application is downloaded - shouldn't be installed
@@ -225,6 +183,14 @@ module.exports = {
           `Release should be downloaded, but not running due to lockfile`
         );
 
+
+        let updatesLocked = await this.context.get().cloud.checkLogsContain(
+          this.context.get().balena.uuid, 
+          `Updates are locked, retrying in 2s...`,           
+        );
+
+        test.ok(updatesLocked, `Update lock message should appear in logs`)
+
         // enable lock override
         await this.context
           .get()
@@ -246,6 +212,12 @@ module.exports = {
           true,
           `Second release should now be running, as override lock was enabled`
         );
+
+        // remove lockfile
+        let removeLockfile = await this.context.get().cloud.executeCommandInContainer(
+          `rm /tmp/balena/updates.lock`, 
+          `main`,
+          this.context.get().balena.uuid)
       },
     },
   ],
