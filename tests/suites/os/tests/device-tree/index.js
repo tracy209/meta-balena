aa8f1bf87e4f67f6d5a443d67c1eeec3833b5b8d
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2021-08-31 09:48:12 +0000

    patch: Add dtoverlay practical test
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

--- tests/suites/os/tests/device-tree/index.js
+++ tests/suites/os/tests/device-tree/index.js
@@ -23,86 +23,131 @@ module.exports = {
 	tests: [
 		{
 			title: 'DToverlay & DTparam tests',
-			run: async function(test) {
+			run: async function (test) {
 				let ip = await this.context.get().worker.ip(this.context.get().link);
 
-				// Wait for supervisor API to start
-				await this.context.get().utils.waitUntil(async () => {
-					return (
-						(await request({
-							method: 'GET',
-							uri: `http://${ip}:${SUPERVISOR_PORT}/ping`,
-						})) === 'OK'
-					);
-				}, false);
+				const exportPin = async (pin) => {
+					return await this.context.get().worker.executeCommandInHostOS(
+						`echo ${pin} >/sys/class/gpio/export`,
+						this.context.get().link
+					)
+				}
+				const unexportPin = async (pin) => {
+					return await this.context.get().worker.executeCommandInHostOS(
+						`echo ${pin} >/sys/class/gpio/unexport`,
+						this.context.get().link
+					)
+				}
 
-				const targetState = {
-					local: {
-						name: 'local',
-						config: {
-							HOST_CONFIG_dtoverlay: '"vc4-fkms-v3d","i2c0","i2c1","i2c3"',
-							HOST_CONFIG_dtparam:
-								'"i2c_arm=on","spi=on","audio=on","foo=bar","level=42"',
-							SUPERVISOR_PERSISTENT_LOGGING: 'true',
-							SUPERVISOR_LOCAL_MODE: 'true',
+				const getPinValue = async (pin) => {
+					return await this.context.get().worker.executeCommandInHostOS(
+						`cat /sys/class/gpio/gpio${pin}/value`,
+						this.context.get().link
+					)
+				}
+
+				const applySupervisorConfig = async (direction) => {
+					// Wait for supervisor API to start
+					await this.context.get().utils.waitUntil(async () => {
+						return (
+							(await request({
+								method: 'GET',
+								uri: `http://${ip}:${SUPERVISOR_PORT}/ping`,
+							})) === 'OK'
+						);
+					}, false);
+
+					const targetState = {
+						local: {
+							name: 'local',
+							config: {
+								HOST_CONFIG_dtoverlay: `"gpio-key,gpio=4,active_low=0,gpio_pull=${direction}"`,
+								HOST_CONFIG_dtparam:
+									'"i2c_arm=on","spi=on","audio=on","foo=bar","level=42"',
+								SUPERVISOR_PERSISTENT_LOGGING: 'true',
+								SUPERVISOR_LOCAL_MODE: 'true',
+							},
+							apps: {},
 						},
-						apps: {},
-					},
-					dependent: {
-						apps: [],
-						devices: [],
-					},
-				};
+						dependent: {
+							apps: [],
+							devices: [],
+						},
+					};
 
-				await this.context
-					.get()
-					.worker.executeCommandInHostOS(
-						'touch /tmp/reboot-check',
-						this.context.get().link,
+					await this.context
+						.get()
+						.worker.executeCommandInHostOS(
+							'touch /tmp/reboot-check',
+							this.context.get().link,
+						);
+
+					// Setting the device tree variables using Supervisor API
+					// This request reboots the DUT
+					const setTargetState = await request({
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						json: true,
+						body: targetState,
+						uri: `http://${ip}:${SUPERVISOR_PORT}/v2/local/target-state`,
+					});
+
+					test.same(
+						setTargetState,
+						{ status: 'success', message: 'OK' },
+						'DToverlay & DTparam configured successfully through Supervisor API',
 					);
 
-				// Setting the device tree variables using Supervisor API
-				// This request reboots the DUT
-				const setTargetState = await request({
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					json: true,
-					body: targetState,
-					uri: `http://${ip}:${SUPERVISOR_PORT}/v2/local/target-state`,
-				});
+					await this.context.get().utils.waitUntil(async () => {
+						test.comment('Waiting for DUT to come back online after reboot...');
+						return (
+							(await this.context
+								.get()
+								.worker.executeCommandInHostOS(
+									'[[ ! -f /tmp/reboot-check ]] && echo "pass"',
+									this.context.get().link,
+								)) === 'pass'
+						);
+					}, false);
 
-				test.same(
-					setTargetState,
-					{ status: 'success', message: 'OK' },
-					'DToverlay & DTparam configured successfully through Supervisor API',
-				);
+					// IP of the device sometimes change after reboots, hence initalising again
+					ip = await this.context.get().worker.ip(this.context.get().link);
 
-				await this.context.get().utils.waitUntil(async () => {
-					test.comment('Waiting for DUT to come back online after reboot...');
-					return (
-						(await this.context
-							.get()
-							.worker.executeCommandInHostOS(
-								'[[ ! -f /tmp/reboot-check ]] && echo "pass"',
-								this.context.get().link,
-							)) === 'pass'
-					);
-				}, false);
+					await this.context.get().utils.waitUntil(async () => {
+						test.comment('Waiting for supervisor to be ready after reboot...');
+						return (
+							(await request({
+								method: 'GET',
+								uri: `http://${ip}:${SUPERVISOR_PORT}/ping`,
+							})) === 'OK'
+						);
+					}, false);
+				}
 
-				// IP of the device sometimes change after reboots, hence initalising again
-				ip = await this.context.get().worker.ip(this.context.get().link);
-
-				await this.context.get().utils.waitUntil(async () => {
-					test.comment('Waiting for supervisor to be ready after reboot...');
-					return (
-						(await request({
-							method: 'GET',
-							uri: `http://${ip}:${SUPERVISOR_PORT}/ping`,
-						})) === 'OK'
-					);
-				}, false);
+				await exportPin(4)
+				if (await getPinValue(4) === "0") {
+					test.equal(await getPinValue(4), "0", "Pin 4 was Low when the test started")
+					console.log(await this.context.get().worker.executeCommandInHostOS("ls /sys/class/gpio/", this.context.get().link))
+					await unexportPin(4)
+					console.log(await this.context.get().worker.executeCommandInHostOS("ls /sys/class/gpio/", this.context.get().link))
+					await applySupervisorConfig("up")
+					console.log(await this.context.get().worker.executeCommandInHostOS("ls /sys/class/gpio/", this.context.get().link))
+					await exportPin(4)
+					console.log(await this.context.get().worker.executeCommandInHostOS("ls /sys/class/gpio/", this.context.get().link))
+					test.equal(await getPinValue(4), "1", "Pin 4 is set to High after applying dtoverlay")
+					await unexportPin(4)
+					console.log(await this.context.get().worker.executeCommandInHostOS("ls /sys/class/gpio/", this.context.get().link))
+				} else {
+					test.equal(await getPinValue(4), "1", "Pin 4 is High as expected")
+					await unexportPin(4)
+					await applySupervisorConfig("down")
+					await exportPin(4)
+					console.log(await this.context.get().worker.executeCommandInHostOS("ls /sys/class/gpio/", this.context.get().link))
+					test.equal(await getPinValue(4), "0", "Pin 4 is set to Low after applying dtoverlay")
+					await unexportPin(4)
+				}
 
 				// Get the current target state of device
 				const currentState = await request({
